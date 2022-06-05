b26f63024564ffc26cab721bc8f6f3f8cf83f064
adimasmudi <adimasmudi@gmail.com>
2022-06-05 10:51:44 +0700

    all CRUD functionality is done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -54,4 +54,39 @@ class ListingController extends Controller
 
         return redirect('/')->with('message', 'Listing berhasil dibuat!!');
     }
+
+    // Show edit form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    // update listing data
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            "title" => "required",
+            "company" => ["required"],
+            "location" => "required",
+            "email" => ["required", "email"],
+            "website" => "required",
+            "tags" => "required",
+            "description" => "required"
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing berhasil diedit!!');
+    }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+
+        return redirect('/')->with('message', 'Listing berhasil dihapus');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,11 @@ Route::post('/listings', [ListingController::class, 'store']);
 // Show Edit Form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
-
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 
 // Single Listing
